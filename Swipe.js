22e8207f2a1a97302fede239c453e932ee7dcf68
verodant <=>
2019-02-25 21:00:17 +0100

    Ajustes

--- Swipe.js
+++ Swipe.js
@@ -13,34 +13,34 @@ export class Swipe {
   }
 
   _swipeAction(entry) {
-    const direction = entry.boundingClientRect.left > 0 ? "left" : "right";
     this.observer.unobserve(entry.target);
-    const pageToRemove = direction == "left" ? 2 : 0;
 
-    console.log(pageToRemove);
+    const DIRECTION = entry.boundingClientRect.left > 0 ? "left" : "right";
+    const pageToRemove = DIRECTION == "left" ? 2 : 0; /*TODO estos valores los calcularemos con cuantos swipes queremos precargar*/ 
+
     this.container.removeChild(this.container.children[pageToRemove]);
 
     let el = document.createElement("div");
+    el.classList.add("container-page");
+
     let img = new Image();
     img.src = "https://picsum.photos/411/731/?random&m=" + Math.random();
     img.style.height = "100%";
     img.style.width = "100%";
+    
     el.appendChild(img);
-    el.classList.add("container-page");
-
-    const actual = this.container.children[direction == "left" ? 0 : 1];
+    
+    const actual = this.container.children[DIRECTION == "left" ? 0 : 1];
+    
     setTimeout(_ => {
-      if (direction == "left") {
+      if (DIRECTION == "left") {
         this.container.insertBefore(el, actual);
-        this.container.classList.remove("smooth");
         actual.scrollIntoView({
           behavior: "instant"
-        });
-        this.container.classList.add("smooth");
+        });   
       } else {
         this.container.appendChild(el);
       }
-
       this.observer.observe(actual);
     }, 50);
   }
@@ -56,8 +56,7 @@ export class Swipe {
       rootMargin: "100%",
       threshold: [0, 1]
     };
-    const callback = entries =>
-      entries.forEach(this._entryAction.bind(this, ...arguments));
+    const callback = entries => entries.forEach(this._entryAction.bind(this, ...arguments));
     this.observer = new IntersectionObserver(callback, options);
     this.observer.observe(swipeContainer.children[1]);
   }
